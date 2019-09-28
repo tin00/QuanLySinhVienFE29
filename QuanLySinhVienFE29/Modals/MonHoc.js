4c17f46af3532098e0fad9f48744b0338b24a8d6
nttin256 <nttin256@gmail.com>
2019-09-28 20:48:14 +0700

    Tin leader dep troai -- commit

--- QuanLySinhVienFE29/Modals/MonHoc.js
+++ QuanLySinhVienFE29/Modals/MonHoc.js
@@ -1,9 +1,6 @@
 function MonHoc() {
     this.monVan = '';
     this.monHoa = '';
-<<<<<<< HEAD
-}
-=======
     this.monDia = '';
     this.monToan = '';
->>>>>>> dad50dd8ae83e8ea9b932e2cf1361e3852504ce4
+}
