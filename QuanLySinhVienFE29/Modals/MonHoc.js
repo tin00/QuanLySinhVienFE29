dad50dd8ae83e8ea9b932e2cf1361e3852504ce4
unknown <khanguyen461995@gmail.com>
2019-09-28 20:44:27 +0700

    Kha them mon Toan

--- QuanLySinhVienFE29/Modals/MonHoc.js
+++ QuanLySinhVienFE29/Modals/MonHoc.js
@@ -2,9 +2,4 @@ function MonHoc() {
     this.monVan = '';
     this.monHoa = '';
     this.monDia = '';
-<<<<<<< HEAD
     this.monToan = '';
-=======
-    this.Ly = "";
->>>>>>> 77585b589e2d65a637ebe4ebb824e06998d8e7be
-}
